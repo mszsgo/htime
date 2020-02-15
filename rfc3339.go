3dc84f74af08879accac0316f3547d30a2cfcc15
zengs <zengs@unionlive.com>
2020-02-15 21:57:02 +0800

    rename rfc3339

--- rfc3339.go
+++ rfc3339.go
@@ -6,11 +6,11 @@ import (
 	"time"
 )
 
-func NowRFC3339() string {
+func RFC3339() string {
 	return time.Now().Format(time.RFC3339)
 }
 
-func FormatRFC3339(t time.Time) string {
+func FRFC3339(t time.Time) string {
 	return t.Local().Format(time.RFC3339)
 }
 
